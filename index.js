d47811a788090bbfb14d582eab86172b8d02a112
Empairim <titusfaulknertull@yahoo.com>
2023-12-29 16:45:30 -0500

    created a player class

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const canvas = document.querySelector("canvas");
-
 const ctx = canvas.getContext("2d");
 
-console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 50;
+    this.height = 50;
+  }
+  //draws the rectangle  takes 4 args x,y,with,height
+  draw() {
+    ctx.fillStyle = "aqua";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const hero = new Player();
+hero.draw();
