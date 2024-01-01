ff1a202449751591ddf3d3f7ec22ce62f01e4141
Empairim <titusfaulknertull@yahoo.com>
2024-01-01 16:03:02 -0500

    refactored ALOT of code to clean up the main game function

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 //          CANVAS SIZE
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 800;
+canvas.height = 470;
 
 // import grassPlatform from "./Assets/Tiles/grass.png";
 // console.log(grassPlatform);
 //        CONSTANTS
 const gravity = 0.5;
 const heroSpeed = 5;
-const keys = {
-  right: {
-    pressed: false,
-  },
-  left: {
-    pressed: false,
-  },
-};
 
+//                            CLASSES
 //          PLAYER LOGIC
 class Player {
   constructor() {
@@ -31,11 +24,18 @@ class Player {
     };
     this.width = 30;
     this.height = 30;
+    this.image = document.querySelector("#hero-img");
   }
   //draws the rectangle  takes 4 args x,y,width,height
   draw() {
-    ctx.fillStyle = "aqua";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    // ctx.fillStyle = "aqua";
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
   }
   //to change player functions over time
   update() {
@@ -54,24 +54,15 @@ class Player {
 
 //      PLATOFRM LOGIC
 
-// const platformImages = [
-
-//   "./Assets/Tiles/grass.png",
-//   "./Assets/Tiles/stone.png",
-//   "./Assets/Tiles/box.png",
-//   "./Assets/Tiles/bridge.png",
-//   "./Assets/Tiles/dirtHalf.png",
-
-// ] post MVP
 class Platform {
-  constructor({ x, y }) {
+  constructor({ x, y, image }) {
     this.position = {
       x, // 200, was hard coded at first to test
       y, // 200,
     };
     this.width = 200;
     this.height = 25;
-    this.image = document.querySelector("#platform-image");
+    this.image = document.querySelector("#platform-img");
   }
   draw() {
     // ctx.fillStyle = "red";
@@ -84,68 +75,94 @@ class Platform {
     );
   }
 }
+//    COLLISION LOGIC
+
+class CollisionManager {
+  static handleCollisions(player, platforms) {
+    platforms.forEach((platform) => {
+      if (
+        //❕1:check if bottom of hero object is same level as top of platform object position.y starts at end using .height to find the feet/bottom of hero.
+        player.position.y + player.height <= platform.position.y &&
+        //❕2:checks if hero velocity is below or at same level as top of platform.pretty much checking if in near future will direction hero is moving in will match platforms position to check for collision.
+        player.position.y + player.height + player.velocity.y >=
+          platform.position.y &&
+        //❕3:checks if right edge of hero is to right or at same position as left edge of platform.Same as with height but for the x axis
+        player.position.x + player.width >= platform.position.x &&
+        //❕4:checks if left edge of hero is to left or at same position as right edge of platform.
+        player.position.x <= platform.position.x + platform.width
+      ) {
+        //5: stops vertical veclocity if conditons are met
+        player.velocity.y = 0;
+      }
+    });
+  }
+}
+
+//    PLAYER CONTROLLER LOGIC
+class PlayerController {
+  constructor() {
+    this.hero = hero;
+    this.platforms = platforms;
+    this.heroSpeed = heroSpeed;
+    this.keys = {
+      right: {
+        pressed: false,
+      },
+      left: {
+        pressed: false,
+      },
+    };
+  }
+  handleKeyInputs() {
+    if (this.keys.right.pressed && hero.position.x < 400) {
+      hero.velocity.x = heroSpeed;
+    } else if (this.keys.left.pressed && hero.position.x > 100) {
+      hero.velocity.x = -heroSpeed;
+    } else {
+      hero.velocity.x = 0;
+      // gives the illusion of movement
+      if (this.keys.right.pressed) {
+        scrollOffSet += heroSpeed;
+        platforms.forEach((platform) => {
+          platform.position.x -= heroSpeed;
+        });
+      } else if (this.keys.left.pressed) {
+        scrollOffSet -= heroSpeed;
+        platforms.forEach((platform) => {
+          platform.position.x += heroSpeed;
+        });
+      }
+    }
+  }
+}
+
+//                         CLASS INSTANCES
+const hero = new Player();
+const platforms = [
+  new Platform({ x: 200, y: 100 }), // thats why set it as object above
+  new Platform({ x: 400, y: 200 }),
+];
+const playerController = new PlayerController(hero, platforms, heroSpeed);
 
 let scrollOffSet = 0; // WIN SCENARIO
 //            GAME RUN FUNCTION
+
 // this loop is pretty much how tvs works just keep flickering a still image so fast it looks like its actually moving
 function animate() {
   requestAnimationFrame(animate); //recursive loop so the game will keep looping like how we'd do a while loop in pygame
 
   ctx.clearRect(0, 0, canvas.width, canvas.height); //clears canvas 4 args x,y,w,h
-  hero.update();
+
   platforms.forEach((platform) => {
     platform.draw();
   });
-
-  /// HANDLES KEY OBJECT⏫ AND SETS BORDER FOR HERO/PLAYER
-  if (keys.right.pressed && hero.position.x < 400) {
-    hero.velocity.x = heroSpeed;
-  } else if (keys.left.pressed && hero.position.x > 100) {
-    hero.velocity.x = -heroSpeed;
-  } else {
-    hero.velocity.x = 0;
-    // gives the illusion of movement
-    if (keys.right.pressed) {
-      scrollOffSet += heroSpeed;
-      platforms.forEach((platform) => {
-        platform.position.x -= heroSpeed;
-      });
-    } else if (keys.left.pressed) {
-      scrollOffSet -= heroSpeed;
-      platforms.forEach((platform) => {
-        platform.position.x += heroSpeed;
-      });
-    }
-  }
-  // console.log(scrollOffSet);
-  //          PLATFORM COLLISION DETECTION
-  platforms.forEach((platform) => {
-    if (
-      //❕1:check if bottom of hero object is same level as top of platform object position.y starts at end using .height to find the feet/bottom of hero.
-      hero.position.y + hero.height <= platform.position.y &&
-      //❕2:checks if hero velocity is below or at same level as top of platform.pretty much checking if in near future will direction hero is moving in will match platforms position to check for collision.
-      hero.position.y + hero.height + hero.velocity.y >= platform.position.y &&
-      //❕3:checks if right edge of hero is to right or at same position as left edge of platform.Same as with height but for the x axis
-      hero.position.x + hero.width >= platform.position.x &&
-      //❕4:checks if left edge of hero is to left or at same position as right edge of platform.
-
-      hero.position.x <= platform.position.x + platform.width
-    ) {
-      //5: stops vertical veclocity if conditons are met
-      hero.velocity.y = 0;
-    }
-  });
+  hero.update();
+  playerController.handleKeyInputs();
+  CollisionManager.handleCollisions(hero, platforms);
   if (scrollOffSet > 1800) {
     console.log("ya win nerd");
   }
 }
-//    CLASS INSTANCES
-const hero = new Player();
-// const platform = new Platform(); was testing building
-const platforms = [
-  new Platform({ x: 200, y: 100 }), // thats why set it as object above
-  new Platform({ x: 400, y: 200 }),
-];
 
 //      RUNNING THE GAME
 animate();
@@ -154,49 +171,41 @@ animate();
 let isJumpKeyPressed = false;
 //                   destuctrue the event object
 addEventListener("keydown", ({ keyCode }) => {
-  // console.log(keyCode);
   switch (keyCode) {
     case 65: // A key LEFT
-      keys.left.pressed = true;
+      playerController.keys.left.pressed = true;
       break;
     case 83: // S key DOWN
       hero.velocity.y += 20;
       break;
-    case 68: // A key RIGHT
-      keys.right.pressed = true;
+    case 68: // D key RIGHT
+      playerController.keys.right.pressed = true;
       break;
-    case 87: // A key UP
+    case 87: // W key UP
       if (!isJumpKeyPressed && hero.velocity.y <= 1) {
         hero.velocity.y -= 20;
         isJumpKeyPressed = true; // Set the flag
         console.log("Jump");
       } else isJumpKeyPressed = false;
-
       break;
-
     default:
       break;
   }
 });
 
 addEventListener("keyup", ({ keyCode }) => {
-  console.log(keyCode);
   switch (keyCode) {
     case 65: // A key LEFT
-      // hero.velocity.x = 0;
-      keys.left.pressed = false;
+      playerController.keys.left.pressed = false;
       break;
     case 83: // S key DOWN
       break;
-    case 68: // A key RIGHT
-      // hero.velocity.x = 0;
-      keys.right.pressed = false;
+    case 68: // D key RIGHT
+      playerController.keys.right.pressed = false;
       break;
-    case 87: // A key UP
+    case 87: // W key UP
       console.log("up");
-
       break;
-
     default:
       break;
   }
